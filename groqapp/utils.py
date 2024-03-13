891e0b17ce3bc66f3b423beb84c30dd3930e8873
fasiloffcial <muhammedfasiloffcial@gmail.com>
2024-03-13 19:59:20 +0530

    switched to django-environ to load .env, added .gitignore file

--- groqapp/utils.py
+++ groqapp/utils.py
@@ -1,8 +1,14 @@
 import os
+import environ
 from groq import Groq
 
+env = environ.Env()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(_file_)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 def get_groq_client():
-    api_key = os.environ.get("GROQ_API_KEY")
+    api_key = env('GROQ_API_KEY')
     if not api_key:
         raise ValueError("Missing GROQ_API_KEY environment variable")
     return Groq(api_key=api_key)
